977c552cad5fe9cb4971c36cab3b63f0107eb7af
Zchappie <zeek.uni@outlook.com>
2020-11-07 18:23:53 +0100

    orb is on, but not well distributed

--- pyslam/pyslam.py
+++ pyslam/pyslam.py
@@ -2,13 +2,22 @@ import cv2
 
 width = 1920//2
 height = 1080//2
+display_name = 'SLAM'
+orb = cv2.ORB_create()
 
 def process_frame(img):
+    # load
     img = cv2.resize(img, (width, height))
-    cv2.imshow('SLAM', img)
-    cv2.waitKey(1)
-    print(img.shape)
 
+    # feature ORB
+    kp, des = orb.detectAndCompute(img, None)
+    for p in kp:
+        u, v = map(lambda x: int(round(x)), p.pt)
+        cv2.circle(img, (u,v), color=(0,255,0), radius=3)
+
+    # display
+    cv2.imshow(display_name, img)
+    cv2.waitKey(1)
 
 if __name__ == "__main__":
     cap = cv2.VideoCapture("test.mp4")
