c1943ad1a598dce1d4ed0ebe1dbab8ebbb470c04
Zchappie <zeek.uni@outlook.com>
2020-11-08 13:36:56 +0100

    doc display file

--- pyslam/display.py
+++ pyslam/display.py
@@ -1,16 +1,20 @@
 import pygame
-from pygame.locals import DOUBLEBUF
+from pygame import DOUBLEBUF
 
 class Display(object):
     def __init__(self, W, H):
-        pygame.init()
-        self.screen = pygame.display.set_mode((W, H), DOUBLEBUF)
-        self.surface = pygame.Surface(self.screen.get_size()).convert()
+        pygame.init() # connect abstractions to specific hardware
+        self.screen = pygame.display.set_mode((W, H), DOUBLEBUF) # canvas
+        self.surface = pygame.Surface(self.screen.get_size()).convert() # convert pxl format
 
     def paint(self, img):
         for event in pygame.event.get():
             pass
 
-        pygame.surfarray.blit_array(self.surface, img.swapaxes(0,1)[:,:,[2,1,0]])
+        # blit directly from array interface
+        pygame.surfarray.blit_array(self.surface, img.swapaxes(0,1)[:,:,[2,1,0]]) # cv2 BGR
+        # blit surface onto screen
         self.screen.blit(self.surface, (0,0))
+        # update the full display surface to the screen
         pygame.display.flip()
+        
